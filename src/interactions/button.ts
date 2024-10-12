47126428a4d2618032077ce8bd78b490be051dea
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2024-10-12 15:46:00 -0700

    feat: Role management

--- src/interactions/button.ts
+++ src/interactions/button.ts
@@ -1,4 +1,4 @@
-import type { ButtonInteraction } from "discord.js";
+import { roleMention, type ButtonInteraction } from "discord.js";
 import { UNVERIFIED_ROLE_ID, VERIFIED_ROLE_ID } from "#util";
 
 export async function handleButton(interaction: ButtonInteraction<"cached">) {
@@ -11,5 +11,19 @@ export async function handleButton(interaction: ButtonInteraction<"cached">) {
             interaction.member.roles.set([...interaction.member.roles.cache.keys(), VERIFIED_ROLE_ID].filter(x => x !== UNVERIFIED_ROLE_ID)),
             interaction.reply({ content: "You have successfully verified!", ephemeral: true })
         ]);
+    } else if (interaction.customId.startsWith("roles-")) {
+        const roleId = interaction.customId.split("-")[1];
+
+        if (interaction.member.roles.cache.has(roleId)) {
+            await Promise.all([
+                interaction.member.roles.remove(roleId),
+                interaction.reply({ content: `Successfully removed from ${roleMention(roleId)}!`, ephemeral: true })
+            ]);
+        } else {
+            await Promise.all([
+                interaction.member.roles.add(roleId),
+                interaction.reply({ content: `Successfully added to ${roleMention(roleId)}!`, ephemeral: true })
+            ]);
+        }
     }
 }
